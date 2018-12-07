434dc565a71492636bb87d6ec64fff80bdebd518
Nathan Daniel Gezahegne <nathandaniel@Nathans-MacBook-Pro.local>
2018-12-07 11:28:07 -0500

    working condition before seed adjustment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,10 @@ class UsersController < ApplicationController
   def show
     render json: User.all.find_by_id(params[:id])
   end
+
+  def find_user_by_name
+    name = params[:name].split('_').join(' ')
+    # byebug
+    render json: User.all.find_by(name: name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :reservations, :restaurants, :users
+
+  get 'find_user/:name', to: 'users#find_user_by_name'
 end
